8d4ba98d8630f5b4ea9bcc9cb121a688e5a16e27
Abdelrahman Naser <a.naser.cse@gmail.com>
2023-10-22 04:19:21 +0300

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6092786649:AAGsiM_0resGLglZTtRc-9iJtod2TagIhNE"
 bot_user_name = "@UT12CheckerBot"
-URL = "https://webhooktestbot.onrender.com"
+URL = "https://webhookbottest.streamlit.app"
